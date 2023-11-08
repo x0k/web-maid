ee96f07ee42877c533f75e74e75113379cd800df
Krasilnikov Roman <x0kill@ya.ru>
2023-11-08 21:59:20 +0300

    WIP

--- src/lib/operator.ts
+++ src/lib/operator.ts
@@ -1,15 +1,17 @@
-import type { TypeOf, ZodType } from "zod";
+import { z, type TypeOf, type ZodType } from "zod";
 
-import { traverseJsonLike } from "@/lib/json-like-traverser";
-import { isFunction, isObject } from "@/lib/guards";
+import { isFunction, isObject, isRecord } from "@/lib/guards";
 
 export type Ast<T> = T | Array<Ast<T>> | { [k: string]: Ast<T> };
 
 export type ResolvedAst<R> = R | Array<R> | Record<string, R>;
 
-export type AstVisitor<T, R> = (value: T | ResolvedAst<R>) => R;
+export type AstVisitor<T, R> = (value: T | ResolvedAst<R>) => Promise<R>;
 
-async function traverseAst<T, R>(visitor: AstVisitor<T, R>, value: Ast<T>) {
+async function traverseAst<T, R>(
+  visitor: AstVisitor<T, R>,
+  value: Ast<T>
+): Promise<R> {
   if (Array.isArray(value)) {
     const tmp = new Array<Promise<R>>(value.length);
     for (let i = 0; i < value.length; i++) {
@@ -37,107 +39,77 @@ export const OPERATOR_KEY = "$op";
 
 export type Result<R> = R | Promise<R>;
 
-export interface ExecutionContext<R> {
-  functions: Record<string, Op<R>>;
-  constants: Record<string, OpOrVal<R>>;
-  value: R;
-  stack: string[];
+export type Op<T, R> = (input: T) => Result<R>;
+
+export type OpOrVal<T, R> = R | Op<T, R>;
+
+export type Factory<C, R> = (config: C) => R;
+
+export type OpFactoryConfig<T, R> = Record<string, OpOrVal<T, R>>;
+
+export type OpFactory<C, T, R> = Factory<C, Op<T, R>>;
+
+export async function evalOnContext<T, R>(
+  value: Ast<OpOrVal<T, R>>,
+  context: T
+): Promise<R> {
+  // @ts-expect-error ouch
+  return Promise.resolve(
+    // @ts-expect-error ouch2
+    traverseAst((v) => {
+      if (isFunction<Op<T, R>>(v)) {
+        return v(context);
+      }
+      return v;
+    }, value)
+  );
 }
 
-export type Op<R> = (context: ExecutionContext<R>) => Result<R>;
-
-export type OpOrVal<R> = R | Op<R>;
-
-export type OpFactoryConfig<R> = Record<string, OpOrVal<R>>;
-
-export async function evalOnContext2<R>(
-  value: Ast<R | Op<R>>,
-  context: ExecutionContext<R>
-) {
-  return traverseAst((v: ResolvedAst<R>) => {
-    if (isFunction(v)) {
-      return v(context);
-    }
-    return v;
-  }, value);
-}
-
-export type Factory<T, R> = (value: T) => R;
-
-export function withValidation<S extends ZodType, R, T = unknown>(
+export function withSchema<S extends ZodType, T, R>(
   schema: S,
-  factory: Factory<TypeOf<S>, R>
-): Factory<T, R> {
-  return (value) => factory(schema.parse(value));
+  factory: OpFactory<TypeOf<S>, T, R>
+) {
+  return (value: T) => factory(schema.parse(value));
 }
 
-export type OpFactory<R> = Factory<OpFactoryConfig<R>, Op<R>>;
+export interface Scope<R> {
+  functions: Record<string, Op<Scope<R>, R>>;
+  constants: Record<string, OpOrVal<Scope<R>, R>>;
+  value: R;
+}
 
-export type OpExecutorFactory<R> = Factory<OpFactoryConfig<R>, OpExecutor<R>>;
-
-export function withContext<R>(
-  name: string,
-  factory: OpFactory<R>
-): OpExecutorFactory<R> {
-  return (config) => {
-    const op = factory(config);
-    return async (context) => ({
-      ...context,
-      context: await op(context.context),
-      stack: context.stack.concat(name),
-    });
+function mergeScopes<R>(a: Scope<R>, b: Partial<Scope<R>>): Scope<R> {
+  return {
+    value: a.value,
+    functions: b.functions ? { ...a.functions, ...b.functions } : a.functions,
+    constants: b.constants ? { ...a.constants, ...b.constants } : a.constants,
   };
 }
 
-export function evalOnContext(value: OpOrVal, context: OpOrVal) {
-  return traverseJsonLike((v: OpOrVal) => {
-    if (typeof v === "function") {
-      return v(context);
-    }
-    return v;
-  }, value);
-}
+const defineConfig = z.object({
+  functions: z.record(z.function()).optional(),
+  constants: z.record(z.unknown()).optional(),
+  for: z.unknown(),
+});
 
-export abstract class AutoFactory<Z extends ZodType> implements OpFactory {
-  abstract readonly schema: Z;
-
-  protected abstract exec(value: TypeOf<this["schema"]>): OpOrVal;
-
-  Create(config: OpFactoryConfig): Op {
-    return (context) => {
-      const resolvedConfig = evalOnContext(config, context);
-      return this.exec(this.schema.parse(resolvedConfig));
+export const defineOperatorFactory = withSchema(
+  defineConfig,
+  ({ functions, constants, for: action }) => {
+    return async (context: Scope<unknown>) => {
+      let evaluatedConstants: Record<string, unknown> | undefined;
+      if (constants) {
+        const data = await evalOnContext(constants, context);
+        if (isRecord(data)) {
+          evaluatedConstants = data;
+        } else {
+          throw new Error("Constants must be an object");
+        }
+      }
+      const newScope = mergeScopes(context, {
+        functions,
+        constants: evaluatedConstants,
+      });
+      return evalOnContext(action, newScope);
     };
   }
-}
-
-export function composedFactory(
-  factories: Record<string, OpFactory>
-): OpFactory {
-  return {
-    Create(value) {
-      const name = value[OPERATOR_KEY];
-      if (typeof name !== "string") {
-        throw new Error(`Invalid operator name: ${name}`);
-      }
-      const f = factories[name];
-      if (!f) {
-        throw new Error(`Unknown operator: ${name}`);
-      }
-      return f.Create(value);
-    },
-  };
-}
-
-export function makeOperatorResolver(factory: OpFactory) {
-  return (context: OpOrVal) => {
-    if (
-      typeof context === "object" &&
-      context !== null &&
-      OPERATOR_KEY in context
-    ) {
-      return factory.Create(context);
-    }
-    return context;
-  };
-}
+);
