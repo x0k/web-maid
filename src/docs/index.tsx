a8091ac80303dd42cda9e73a5acf460c16b256d2
Krasilnikov Roman <x0kill@ya.ru>
2023-11-21 14:58:00 +0300

    Fix docs page

--- src/docs/index.tsx
+++ src/docs/index.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Docs } from "@/shared/config/docs";
 import "@/shared/styles.css";
 
 const root = document.getElementById("root")!;
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <div className="max-w-3xl mx-auto py-8">
-      <Docs />
+      <QueryClientProvider client={client}>
+        <Docs />
+      </QueryClientProvider>
     </div>
   </React.StrictMode>
 );
